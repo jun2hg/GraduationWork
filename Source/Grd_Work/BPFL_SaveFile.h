79ccd8401d463b192a1a06ec4c38289e8b75d763
jun2hg <tbd05042@gmail.com>
2024-06-04 15:42:22 +0900

    feat BPFL_SaveFile(C++ class)

---
+++ Source/Grd_Work/BPFL_SaveFile.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "BPFL_SaveFile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GRD_WORK_API UBPFL_SaveFile : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable)
+	static bool SaveTextToFile(const FString& Text, const FString& FileName);
+	
+};
